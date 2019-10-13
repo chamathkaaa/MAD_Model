8382d4ab222166ed1a95cf704811a2097cb9761a
UviniC <uviniwijesinghe@gmail.com>
2019-10-13 23:50:16 +0530

    delete/readAll

--- app/src/main/java/com/example/modelpaper/Database/DBHelper.java
+++ app/src/main/java/com/example/modelpaper/Database/DBHelper.java
@@ -2,8 +2,13 @@ package com.example.modelpaper.Database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -54,5 +59,138 @@ public class DBHelper extends SQLiteOpenHelper {
         long newRowId = db.insert(UserProfile.Users.TABLE_NAME, null, values);
 
         return newRowId;
+    }
+
+    public boolean updateInfor(String username, String dob, String password, String gender){
+
+        SQLiteDatabase db = getWritableDatabase();
+
+        // New value for one column
+        ContentValues values = new ContentValues();
+        values.put(UserProfile.Users.COLUMN_1, username);
+        values.put(UserProfile.Users.COLUMN_2, dob);
+        values.put(UserProfile.Users.COLUMN_3, password);
+        values.put(UserProfile.Users.COLUMN_4, gender);
+
+
+        // Which row to update, based on the title
+        String selection = UserProfile.Users.COLUMN_1 + " LIKE ?";
+
+
+
+        String[] selectionArgs = { "username" };
+
+        int count = db.update(
+                UserProfile.Users.TABLE_NAME,
+                values,
+                selection,
+                selectionArgs);
+
+        if(count > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void deleteInfo(String username){
+
+        SQLiteDatabase db = getWritableDatabase();
+
+        // Define 'where' part of query.
+        String selection = UserProfile.Users.COLUMN_1 + " LIKE ?";
+
+        // Specify arguments in placeholder order.
+        String[] selectionArgs = { "username" };
+        // Issue SQL statement.
+        int deletedRows = db.delete(UserProfile.Users.TABLE_NAME, selection, selectionArgs);
+
+    }
+
+    public List readAllInfor(){
+
+        SQLiteDatabase db = getReadableDatabase();
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                BaseColumns._ID,
+                UserProfile.Users.COLUMN_1,
+                UserProfile.Users.COLUMN_2,
+                UserProfile.Users.COLUMN_3,
+                UserProfile.Users.COLUMN_4
+        };
+
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                UserProfile.Users.COLUMN_1 + " ASC";
+
+        Cursor cursor = db.query(
+                UserProfile.Users.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                null,              // The columns for the WHERE clause
+                null,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
+
+        List usernames = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            String user = cursor.getString(
+                    cursor.getColumnIndexOrThrow(UserProfile.Users.COLUMN_1));
+            usernames.add(user);
+        }
+        cursor.close();
+
+        return usernames;
+    }
+
+    public List readAllInfor(String username){
+
+        SQLiteDatabase db = getReadableDatabase();
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                BaseColumns._ID,
+                UserProfile.Users.COLUMN_1,
+                UserProfile.Users.COLUMN_2,
+                UserProfile.Users.COLUMN_3,
+                UserProfile.Users.COLUMN_4
+        };
+
+        // Filter results WHERE "title" = 'My Title'
+        String selection = UserProfile.Users.COLUMN_1 + " = LIKE ?";
+        String[] selectionArgs = { "username" };
+
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                UserProfile.Users.COLUMN_1 + " ASC";
+
+        Cursor cursor = db.query(
+                UserProfile.Users.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
+
+        List userInfo = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            String user = cursor.getString(cursor.getColumnIndexOrThrow(UserProfile.Users.COLUMN_1));
+            String dob = cursor.getString(cursor.getColumnIndexOrThrow(UserProfile.Users.COLUMN_2));
+            String password = cursor.getString(cursor.getColumnIndexOrThrow(UserProfile.Users.COLUMN_3));
+            String gender = cursor.getString(cursor.getColumnIndexOrThrow(UserProfile.Users.COLUMN_4));
+            userInfo.add(user);
+            userInfo.add(dob);
+            userInfo.add(password);
+            userInfo.add(gender);
+        }
+        cursor.close();
+
+        return userInfo;
     }
 }
